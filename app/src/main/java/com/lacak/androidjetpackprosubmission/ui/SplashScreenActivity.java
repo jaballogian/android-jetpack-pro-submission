c3767a92970e2294052605520c19a12970962561
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 16:50:53 +0700

    add Splash Screen Activity to the app and make it as the first launched activity

---
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/SplashScreenActivity.java
@@ -0,0 +1,31 @@
+package com.lacak.androidjetpackprosubmission.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.lacak.androidjetpackprosubmission.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIMED_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+
+                Intent toActivityMain = new Intent(SplashScreenActivity.this, MainActivity.class);
+                toActivityMain.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(toActivityMain);
+                finish();
+            }
+        },SPLASH_TIMED_OUT);
+    }
+}
