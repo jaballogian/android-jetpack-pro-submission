e500c56e79c3cca257ab40856ff96b3fe13555f1
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 23:13:41 +0700

    add on click listener for list item in Film Adapter

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/detail/DetailActivity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/detail/DetailActivity.java
@@ -8,6 +8,8 @@ import com.lacak.androidjetpackprosubmission.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    public static final String EXTRA_FILM = "extra_film";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmAdapter.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmAdapter.java
@@ -2,6 +2,7 @@ package com.lacak.androidjetpackprosubmission.ui.film;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.lacak.androidjetpackprosubmission.R;
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
 import com.lacak.androidjetpackprosubmission.databinding.ItemFilmBinding;
+import com.lacak.androidjetpackprosubmission.ui.detail.DetailActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -68,6 +70,12 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
 
             int imageResource = context.getResources().getIdentifier(filmEntity.getImagePath(), null, context.getPackageName());
             itemFilmBinding.imageView.setImageDrawable(context.getResources().getDrawable(imageResource));
+
+            itemView.setOnClickListener(v -> {
+                Intent moveToDetailActivity = new Intent(itemView.getContext(), DetailActivity.class);
+                moveToDetailActivity.putExtra(DetailActivity.EXTRA_FILM, filmEntity.getTitle());
+                itemView.getContext().startActivity(moveToDetailActivity);
+            });
         }
     }
 }
