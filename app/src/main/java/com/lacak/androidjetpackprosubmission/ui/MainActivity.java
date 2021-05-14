728a639d59e151df9cfa1f31b47c33c3b84324c4
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 16:13:59 +0700

    move Main Activity into root/ui package

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/MainActivity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/MainActivity.java
@@ -1,8 +1,10 @@
-package com.lacak.androidjetpackprosubmission;
+package com.lacak.androidjetpackprosubmission.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.lacak.androidjetpackprosubmission.R;
 
 public class MainActivity extends AppCompatActivity {
 
