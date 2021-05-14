9c9a078150438acf7289b97ebd2ce3e419b41d2c
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 23:19:31 +0700

    make Film Entity implements Parcelable

--- app/src/main/java/com/lacak/androidjetpackprosubmission/data/FilmEntity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/data/FilmEntity.java
@@ -1,11 +1,10 @@
 package com.lacak.androidjetpackprosubmission.data;
 
-public class FilmEntity {
-    private String title, year, genres, duration, rating, overview, url, imagePath;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-    public String getTitle() {
-        return title;
-    }
+public class FilmEntity implements Parcelable {
+    private String title, year, genres, duration, rating, overview, url, imagePath;
 
     public FilmEntity(String title, String year, String genres, String duration, String rating, String overview, String url, String imagePath){
         this.title = title;
@@ -16,6 +15,10 @@ public class FilmEntity {
         this.overview = overview;
         this.url = url;
         this.imagePath = imagePath;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public void setTitle(String title) {
@@ -77,4 +80,44 @@ public class FilmEntity {
     public void setImagePath(String imagePath) {
         this.imagePath = imagePath;
     }
+
+    protected FilmEntity(Parcel in){
+        title = in.readString();
+        year = in.readString();
+        genres = in.readString();
+        duration = in.readString();
+        rating = in.readString();
+        overview = in.readString();
+        url = in.readString();
+        imagePath = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(title);
+        dest.writeString(year);
+        dest.writeString(genres);
+        dest.writeString(duration);
+        dest.writeString(rating);
+        dest.writeString(overview);
+        dest.writeString(url);
+        dest.writeString(imagePath);
+    }
+
+    public static final Creator<FilmEntity> CREATOR = new Creator<FilmEntity>() {
+        @Override
+        public FilmEntity createFromParcel(Parcel in) {
+            return new FilmEntity(in);
+        }
+
+        @Override
+        public FilmEntity[] newArray(int size) {
+            return new FilmEntity[size];
+        }
+    };
 }
