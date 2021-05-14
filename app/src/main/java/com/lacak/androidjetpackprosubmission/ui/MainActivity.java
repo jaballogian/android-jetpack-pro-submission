9b44f6c33029cbcfc7147df273a9bbd07a61e250
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 17:36:42 +0700

    implement view binding and sections pager adapeter in Main Activity

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/MainActivity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/MainActivity.java
@@ -1,16 +1,42 @@
 package com.lacak.androidjetpackprosubmission.ui;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 import com.lacak.androidjetpackprosubmission.R;
+import com.lacak.androidjetpackprosubmission.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding activityMainBinding;
+
+    @StringRes
+    private final int[] TAB_TITLES = new int[]{
+            R.string.movies,
+            R.string.tv_shows
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        activityMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(activityMainBinding.getRoot());
+
+        ViewPager viewPager = activityMainBinding.viewPager;
+        TabLayout tabLayout = activityMainBinding.tabs;
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this);
+
+        viewPager.setAdapter(sectionsPagerAdapter);
+        new TabLayoutMediator(tabLayout, viewPager, ((tab, position) -> tab.setText(getResources().getString(TAB_TITLES[position]))));
+
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
     }
 }
