2805b4166b307ddf37d45d2196c54c08708698a2
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 17:25:38 +0700

    modify Film Fragment to support Tab Layout in Main Activity with only one fragment

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/FilmFragment.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/FilmFragment.java
@@ -2,11 +2,14 @@ package com.lacak.androidjetpackprosubmission.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.lacak.androidjetpackprosubmission.R;
 
@@ -17,44 +20,14 @@ import com.lacak.androidjetpackprosubmission.R;
  */
 public class FilmFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public FilmFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FilmFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FilmFragment newInstance(String param1, String param2) {
+    public static FilmFragment newInstance(int index) {
         FilmFragment fragment = new FilmFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+        Bundle bundle = new Bundle();
+        bundle.putInt(ARG_SECTION_NUMBER, index);
+        fragment.setArguments(bundle);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +35,16 @@ public class FilmFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_film, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final TextView textView = view.findViewById(R.id.text_view_fragment);
+        int index = 1;
+        if (getArguments() != null) {
+            index = getArguments().getInt(ARG_SECTION_NUMBER);
+            textView.setText("This is tab " + index);
+        }
     }
 }
