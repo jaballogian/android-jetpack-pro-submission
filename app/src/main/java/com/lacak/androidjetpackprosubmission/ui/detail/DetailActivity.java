7fc1082d918f5b5bc720449ef94d199b076ee2f7
jaballogian <jaballogianrma77@gmail.com>
2021-05-14 23:29:39 +0700

    view the film entity data in Detail Activity

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/detail/DetailActivity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/detail/DetailActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.lacak.androidjetpackprosubmission.R;
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
+import com.lacak.androidjetpackprosubmission.databinding.ActivityDetailBinding;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_FILM = "extra_film";
+    private ActivityDetailBinding activityDetailBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        activityDetailBinding = ActivityDetailBinding.inflate(getLayoutInflater());
+        setContentView(activityDetailBinding.getRoot());
 
         FilmEntity filmEntity = getIntent().getParcelableExtra(EXTRA_FILM);
+
+        activityDetailBinding.textViewTitle.setText(filmEntity.getTitle());
+        activityDetailBinding.textViewGenres.setText(filmEntity.getGenres());
+        activityDetailBinding.textViewYear.setText(filmEntity.getYear());
+        activityDetailBinding.textViewRating.setText(filmEntity.getRating());
+        activityDetailBinding.textViewDuration.setText(filmEntity.getDuration());
+        activityDetailBinding.textViewOverview.setText(filmEntity.getOverview());
+
+        int imageResource = getResources().getIdentifier(filmEntity.getImagePath(), null, getPackageName());
+        activityDetailBinding.imageView.setImageDrawable(getResources().getDrawable(imageResource));
     }
 }
