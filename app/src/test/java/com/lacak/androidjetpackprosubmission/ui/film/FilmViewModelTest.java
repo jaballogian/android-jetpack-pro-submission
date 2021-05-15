1e08cc135521d13e29267c3018d8abcd18e9b004
jaballogian <jaballogianrma77@gmail.com>
2021-05-15 12:15:45 +0700

    add other test scenarios in Film VIew Model Test and update the test scenarios file

--- app/src/test/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModelTest.java
+++ app/src/test/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModelTest.java
@@ -3,8 +3,10 @@ package com.lacak.androidjetpackprosubmission.ui.film;
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -20,15 +22,40 @@ public class FilmViewModelTest {
 
     @Test
     public void getMoviesListData() {
+        // TEST WITH FULL DATA
         List<FilmEntity> moviesList = filmViewModel.getMoviesListData();
         assertNotNull(moviesList);
         assertEquals(10, moviesList.size());
+
+        // TEST WITH HALF DATA
+        List<FilmEntity> halfMoviesList = filmViewModel.getMoviesListData().subList(0, 5);
+        assertNotNull(halfMoviesList);
+        assertEquals(5, halfMoviesList.size());
+
+        // TEST WITH ZERO DATA
+        List<FilmEntity> zeroMoviesList = filmViewModel.getMoviesListData().subList(0, 0);
+        assertNotNull(zeroMoviesList);
+        assertEquals(0, zeroMoviesList.size());
     }
 
     @Test
     public void getShowsListData() {
+        // TEST WITH FULL DATA
         List<FilmEntity> showsList = filmViewModel.getShowsListData();
         assertNotNull(showsList);
         assertEquals(10, showsList.size());
+
+        // TEST WITH HALF DATA
+        List<FilmEntity> halfShowsList = filmViewModel.getShowsListData().subList(0, 5);
+        assertNotNull(halfShowsList);
+        assertEquals(5, halfShowsList.size());
+
+        // TEST WITH ZERO DATA
+        List<FilmEntity> zeroShowsList = filmViewModel.getShowsListData().subList(0, 0);
+        assertNotNull(zeroShowsList);
+        assertEquals(0, zeroShowsList.size());
     }
+
+
+
 }
--- app/src/test/test_scenarios.md
+++ app/src/test/test_scenarios.md
@@ -5,10 +5,14 @@
 1. Load a list of Movies:
 * Ensure that the list is not null
 * Ensure that the list has the correct size/length
+* Test the list with half of the list size
+* Test the list with zero of the list size
 
 2. Load a list of TV Shows:
 * Ensure that the list is not null
 * Ensure that the list has the correct size/length
+* Test the list with half of the list size
+* Test the list with zero of the list size
 
 ### Detail View Model Test
 
