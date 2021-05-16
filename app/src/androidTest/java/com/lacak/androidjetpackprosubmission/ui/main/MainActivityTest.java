e4db5b54d3c92f6f82241f7a44680bab45c9a3c0
jaballogian <jaballogianrma77@gmail.com>
2021-05-16 17:20:43 +0700

    add Main Activity Test for instrumentation testing

---
+++ app/src/androidTest/java/com/lacak/androidjetpackprosubmission/ui/main/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.lacak.androidjetpackprosubmission.ui.main;
+
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import com.lacak.androidjetpackprosubmission.R;
+import com.lacak.androidjetpackprosubmission.data.FilmEntity;
+import com.lacak.androidjetpackprosubmission.utils.MoviesListDataGenerator;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+    private List<FilmEntity> sampleMoviesList = MoviesListDataGenerator.generateMoviesListData();
+
+    @Rule
+    public ActivityScenarioRule activityScenarioRule = new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void loadFilm(){
+        onView(withId(R.id.recyler_view_film)).check(matches(isDisplayed()));
+        onView(withId(R.id.recyler_view_film)).perform(RecyclerViewActions.scrollToPosition(sampleMoviesList.size()/2 - 1)); // HALF INDEX = INDEX 4
+        onView(withId(R.id.recyler_view_film)).perform(RecyclerViewActions.scrollToPosition(sampleMoviesList.size() - 1)); // LAST INDEX = INDEX 9
+    }
+
+}
