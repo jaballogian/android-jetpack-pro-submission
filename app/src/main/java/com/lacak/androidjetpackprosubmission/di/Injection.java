93a9105ba7c17e31097fb02488f283b1fec4516d
jaballogian <jaballogianrma77@gmail.com>
2021-05-29 11:05:48 +0700

    add Injection class file

---
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/di/Injection.java
@@ -0,0 +1,14 @@
+package com.lacak.androidjetpackprosubmission.di;
+
+import android.content.Context;
+
+import com.lacak.androidjetpackprosubmission.data.source.MainRepository;
+import com.lacak.androidjetpackprosubmission.data.source.remote.RemoteDataSource;
+import com.lacak.androidjetpackprosubmission.utils.JsonHelper;
+
+public class Injection {
+    public static MainRepository provideRepository(Context context){
+        RemoteDataSource remoteDataSource = RemoteDataSource.getInstance(new JsonHelper(context));
+        return MainRepository.getInstance(remoteDataSource);
+    }
+}
