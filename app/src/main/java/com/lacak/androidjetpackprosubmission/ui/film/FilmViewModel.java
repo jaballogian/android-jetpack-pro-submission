19e0b7379ee3c01d694ef2b0b0a19187b07f5c16
jaballogian <jaballogianrma77@gmail.com>
2021-05-29 10:23:44 +0700

    implement Main Repository to get movies and shows lists data in Film View Model class file

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModel.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModel.java
@@ -3,18 +3,23 @@ package com.lacak.androidjetpackprosubmission.ui.film;
 import androidx.lifecycle.ViewModel;
 
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
-import com.lacak.androidjetpackprosubmission.utils.MoviesListDataGenerator;
-import com.lacak.androidjetpackprosubmission.utils.ShowsListDataGenerator;
+import com.lacak.androidjetpackprosubmission.data.source.MainRepository;
 
 import java.util.List;
 
 public class FilmViewModel extends ViewModel {
 
+    private MainRepository mainRepository;
+
+    public FilmViewModel(MainRepository mainRepository){
+        this.mainRepository = mainRepository;
+    }
+
     public List<FilmEntity> getMoviesListData(){
-        return MoviesListDataGenerator.generateMoviesListData();
+        return mainRepository.getAllMovies();
     }
 
     public List<FilmEntity> getShowsListData(){
-        return ShowsListDataGenerator.generateShowsListData();
+        return mainRepository.getAllShows();
     }
 }
