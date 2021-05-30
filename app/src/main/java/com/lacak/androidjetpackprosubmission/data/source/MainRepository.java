c08ba738cc5539fe5c9758ff6228c21a90e2e2a2
jaballogian <jaballogianrma77@gmail.com>
2021-05-30 15:43:43 +0700

    implement Live Data in all get data functions in Main Repository class file

--- app/src/main/java/com/lacak/androidjetpackprosubmission/data/source/MainRepository.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/data/source/MainRepository.java
@@ -1,6 +1,8 @@
 package com.lacak.androidjetpackprosubmission.data.source;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
 import com.lacak.androidjetpackprosubmission.data.source.remote.RemoteDataSource;
@@ -28,68 +30,12 @@ public class MainRepository implements MainDataSource{
     }
 
     @Override
-    public List<FilmEntity> getAllMovies() {
-        List<FilmResponse> movieResponses = remoteDataSource.getAllMovies();
-        ArrayList<FilmEntity> moviesList = new ArrayList<>();
-        for (FilmResponse response : movieResponses) {
-            FilmEntity movie = new FilmEntity(
-                    response.getTitle(),
-                    response.getYear(),
-                    response.getGenres(),
-                    response.getDuration(),
-                    response.getRating(),
-                    response.getOverview(),
-                    response.getUrl(),
-                    response.getImagePath()
-            );
-            moviesList.add(movie);
-        }
-        return moviesList;
-    }
-
-    @Override
-    public List<FilmEntity> getAllShows() {
-        List<FilmResponse> showResponses = remoteDataSource.getAllShows();
-        ArrayList<FilmEntity> showsList = new ArrayList<>();
-        for (FilmResponse response : showResponses) {
-            FilmEntity show = new FilmEntity(
-                    response.getTitle(),
-                    response.getYear(),
-                    response.getGenres(),
-                    response.getDuration(),
-                    response.getRating(),
-                    response.getOverview(),
-                    response.getUrl(),
-                    response.getImagePath()
-            );
-            showsList.add(show);
-        }
-        return showsList;
-    }
-
-    @Override
-    public FilmEntity getDetailFilm(final String title) {
-        List<FilmResponse> movieResponses = remoteDataSource.getAllMovies();
-        FilmEntity film = null;
-        for (FilmResponse response : movieResponses) {
-            if (response.getTitle().equals(title)) {
-                film = new FilmEntity(
-                    response.getTitle(),
-                    response.getYear(),
-                    response.getGenres(),
-                    response.getDuration(),
-                    response.getRating(),
-                    response.getOverview(),
-                    response.getUrl(),
-                    response.getImagePath());
-            }
-        }
-
-        if(film == null){
-            List<FilmResponse> showResponses = remoteDataSource.getAllShows();
-            for (FilmResponse response : showResponses) {
-                if (response.getTitle().equals(title)) {
-                    film = new FilmEntity(
+    public LiveData<List<FilmEntity>> getAllMovies() {
+        MutableLiveData<List<FilmEntity>> movieResults = new MutableLiveData<>();
+        remoteDataSource.getAllMovies(movieResponses -> {
+            ArrayList<FilmEntity> moviesList = new ArrayList<>();
+            for (FilmResponse response : movieResponses) {
+                FilmEntity movie = new FilmEntity(
                         response.getTitle(),
                         response.getYear(),
                         response.getGenres(),
@@ -97,11 +43,81 @@ public class MainRepository implements MainDataSource{
                         response.getRating(),
                         response.getOverview(),
                         response.getUrl(),
-                        response.getImagePath());
+                        response.getImagePath()
+                );
+                moviesList.add(movie);
+            }
+            movieResults.postValue(moviesList);
+        });
+
+        return movieResults;
+    }
+
+    @Override
+    public LiveData<List<FilmEntity>> getAllShows() {
+        MutableLiveData<List<FilmEntity>> showResults = new MutableLiveData<>();
+        remoteDataSource.getAllShows(showResponses -> {
+            ArrayList<FilmEntity> showsList = new ArrayList<>();
+            for (FilmResponse response : showResponses) {
+                FilmEntity show = new FilmEntity(
+                        response.getTitle(),
+                        response.getYear(),
+                        response.getGenres(),
+                        response.getDuration(),
+                        response.getRating(),
+                        response.getOverview(),
+                        response.getUrl(),
+                        response.getImagePath()
+                );
+                showsList.add(show);
+            }
+            showResults.postValue(showsList);
+        });
+
+        return showResults;
+    }
+
+    @Override
+    public LiveData<FilmEntity> getDetailFilm(final String title) {
+        MutableLiveData<FilmEntity> detailFilmResult = new MutableLiveData<>();
+        remoteDataSource.getAllMovies(movieResponses -> {
+            FilmEntity film = null;
+            for (FilmResponse response : movieResponses) {
+                if (response.getTitle().equals(title)) {
+                    film = new FilmEntity(
+                            response.getTitle(),
+                            response.getYear(),
+                            response.getGenres(),
+                            response.getDuration(),
+                            response.getRating(),
+                            response.getOverview(),
+                            response.getUrl(),
+                            response.getImagePath());
                 }
             }
-            return film;
+            detailFilmResult.postValue(film);
+        });
+
+        if(detailFilmResult == null){
+            remoteDataSource.getAllShows(showResponses -> {
+                FilmEntity film = null;
+                for (FilmResponse response : showResponses) {
+                    if (response.getTitle().equals(title)) {
+                        film = new FilmEntity(
+                                response.getTitle(),
+                                response.getYear(),
+                                response.getGenres(),
+                                response.getDuration(),
+                                response.getRating(),
+                                response.getOverview(),
+                                response.getUrl(),
+                                response.getImagePath());
+                    }
+                }
+                detailFilmResult.postValue(film);
+            });
+
         }
-        return film;
+        return detailFilmResult;
     }
 }
