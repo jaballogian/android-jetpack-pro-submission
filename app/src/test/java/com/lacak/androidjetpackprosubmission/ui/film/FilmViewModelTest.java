c0506e391ffaddc1575b80c7e6947f85ff29c002
jaballogian <jaballogianrma77@gmail.com>
2021-06-01 14:11:08 +0700

    add live data unit test for Film View Model in Film View Model Test file

--- app/src/test/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModelTest.java
+++ app/src/test/java/com/lacak/androidjetpackprosubmission/ui/film/FilmViewModelTest.java
@@ -1,11 +1,16 @@
 package com.lacak.androidjetpackprosubmission.ui.film;
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+
 import com.lacak.androidjetpackprosubmission.data.FilmEntity;
 import com.lacak.androidjetpackprosubmission.data.source.MainRepository;
 import com.lacak.androidjetpackprosubmission.utils.MoviesListDataGenerator;
 import com.lacak.androidjetpackprosubmission.utils.ShowsListDataGenerator;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -22,8 +27,14 @@ public class FilmViewModelTest {
 
     private FilmViewModel filmViewModel;
 
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
     @Mock
     private MainRepository mainRepository;
+
+    @Mock
+    private Observer<List<FilmEntity>> observer;
 
     @Before
     public void setUp(){
@@ -32,19 +43,33 @@ public class FilmViewModelTest {
 
     @Test
     public void getMoviesListData() {
-        when(mainRepository.getAllMovies()).thenReturn(MoviesListDataGenerator.generateMoviesListData());
-        List<FilmEntity> moviesList = filmViewModel.getMoviesListData();
-        verify(mainRepository.getAllMovies());
+        List<FilmEntity> dummyMovies = MoviesListDataGenerator.generateMoviesListData();
+        MutableLiveData<List<FilmEntity>> movies = new MutableLiveData<>();
+        movies.setValue(dummyMovies);
+
+        when(mainRepository.getAllMovies()).thenReturn(movies);
+        List<FilmEntity> moviesList = filmViewModel.getMoviesListData().getValue();
+        verify(mainRepository).getAllMovies();
         assertNotNull(moviesList);
         assertEquals(10, moviesList.size());
+
+        filmViewModel.getMoviesListData().observeForever(observer);
+        verify(observer).onChanged(dummyMovies);
     }
 
     @Test
     public void getShowsListData() {
-        when(mainRepository.getAllShows()).thenReturn(ShowsListDataGenerator.generateShowsListData());
-        List<FilmEntity> showsList = filmViewModel.getShowsListData();
-        verify(mainRepository.getAllShows());
+        List<FilmEntity> dummyShows = ShowsListDataGenerator.generateShowsListData();
+        MutableLiveData<List<FilmEntity>> shows = new MutableLiveData<>();
+        shows.setValue(dummyShows);
+
+        when(mainRepository.getAllShows()).thenReturn(shows);
+        List<FilmEntity> showsList = filmViewModel.getShowsListData().getValue();
+        verify(mainRepository).getAllShows();
         assertNotNull(showsList);
         assertEquals(10, showsList.size());
+
+        filmViewModel.getShowsListData().observeForever(observer);
+        verify(observer).onChanged(dummyShows);
     }
 }
