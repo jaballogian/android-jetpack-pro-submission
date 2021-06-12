40014497327df840e5ef32946eb2de76ae460f55
jaballogian <jaballogianrma77@gmail.com>
2021-06-13 00:03:43 +0700

    change title and color of action bar in Favorite Activity

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/favorite/FavoriteActivity.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/favorite/FavoriteActivity.java
@@ -1,7 +1,9 @@
 package com.lacak.androidjetpackprosubmission.ui.favorite;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import com.lacak.androidjetpackprosubmission.R;
@@ -12,5 +14,9 @@ public class FavoriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(getString(R.string.favorite));
+        actionBar.setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.light_blue_a400)));
     }
 }
