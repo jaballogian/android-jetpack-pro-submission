6f5154c0932da147ab40d3f679ba1ff05a4af5e8
jaballogian <jaballogianrma77@gmail.com>
2021-06-12 22:48:56 +0700

    add Favorite Activity to root/ui/favorite package

---
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/favorite/FavoriteActivity.java
@@ -0,0 +1,16 @@
+package com.lacak.androidjetpackprosubmission.ui.favorite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lacak.androidjetpackprosubmission.R;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
