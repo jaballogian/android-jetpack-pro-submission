6b2c4194c7461b9c666dfc8d4a23e57d6f26fb2f
jaballogian <jaballogianrma77@gmail.com>
2021-06-13 23:12:33 +0700

    add provide favorite film repository function in Injection

--- app/src/main/java/com/lacak/androidjetpackprosubmission/di/Injection.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/di/Injection.java
@@ -1,7 +1,9 @@
 package com.lacak.androidjetpackprosubmission.di;
 
+import android.app.Application;
 import android.content.Context;
 
+import com.lacak.androidjetpackprosubmission.data.FavoriteFilmRepository;
 import com.lacak.androidjetpackprosubmission.data.MainRepository;
 import com.lacak.androidjetpackprosubmission.data.source.remote.RemoteDataSource;
 import com.lacak.androidjetpackprosubmission.utils.JsonHelper;
@@ -10,5 +12,9 @@ public class Injection {
     public static MainRepository provideRepository(Context context){
         RemoteDataSource remoteDataSource = RemoteDataSource.getInstance(new JsonHelper(context));
         return MainRepository.getInstance(remoteDataSource);
+    }
+
+    public static FavoriteFilmRepository provideFavoriteFilmRepository(Application application){
+        return FavoriteFilmRepository.getInstance(application);
     }
 }
