6e2b92d6390c4ca448b08963ec4e2a188033ff53
jaballogian <jaballogianrma77@gmail.com>
2021-06-13 23:13:50 +0700

    add Favorite Film Repository to constructor and get instance function in View Model Factory

--- app/src/main/java/com/lacak/androidjetpackprosubmission/viewmodel/ViewModelFactory.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/viewmodel/ViewModelFactory.java
@@ -1,11 +1,13 @@
 package com.lacak.androidjetpackprosubmission.viewmodel;
 
+import android.app.Application;
 import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.lacak.androidjetpackprosubmission.data.FavoriteFilmRepository;
 import com.lacak.androidjetpackprosubmission.data.MainRepository;
 import com.lacak.androidjetpackprosubmission.di.Injection;
 import com.lacak.androidjetpackprosubmission.ui.detail.DetailViewModel;
@@ -15,15 +17,17 @@ public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory{
     private static volatile ViewModelFactory INSTANCE;
 
     private final MainRepository mainRepository;
+    private final FavoriteFilmRepository favoriteFilmRepository;
 
-    private ViewModelFactory(MainRepository mainRepository) {
+    private ViewModelFactory(MainRepository mainRepository, FavoriteFilmRepository favoriteFilmRepository) {
         this.mainRepository = mainRepository;
+        this.favoriteFilmRepository = favoriteFilmRepository;
     }
 
-    public static ViewModelFactory getInstance(Context context) {
+    public static ViewModelFactory getInstance(Context context, Application application) {
         if (INSTANCE == null) {
             synchronized (ViewModelFactory.class) {
-                INSTANCE = new ViewModelFactory(Injection.provideRepository(context));
+                INSTANCE = new ViewModelFactory(Injection.provideRepository(context), Injection.provideFavoriteFilmRepository(application));
             }
         }
         return INSTANCE;
@@ -37,7 +41,7 @@ public class ViewModelFactory extends ViewModelProvider.NewInstanceFactory{
         if (modelClass.isAssignableFrom(FilmViewModel.class)) {
             return (T) new FilmViewModel(mainRepository);
         } else if (modelClass.isAssignableFrom(DetailViewModel.class)) {
-            return (T) new DetailViewModel(mainRepository);
+            return (T) new DetailViewModel(mainRepository, favoriteFilmRepository);
         }
 
         throw new IllegalArgumentException("Unknown ViewModel class: " + modelClass.getName());
