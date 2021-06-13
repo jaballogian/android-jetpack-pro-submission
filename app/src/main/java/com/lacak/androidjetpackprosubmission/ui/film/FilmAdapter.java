e63ff5517d1380ce94e839fb1f9838fb56ba225e
jaballogian <jaballogianrma77@gmail.com>
2021-06-14 00:17:02 +0700

    delete film type property from Film Adapter

--- app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmAdapter.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/film/FilmAdapter.java
@@ -18,7 +18,6 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
     private final List<FilmEntity> listFilms = new ArrayList<>();
     private static Context context;
     private static OnItemClickCallback onItemClickCallback;
-    private String filmType = "";
 
     public FilmAdapter(Context context){
         this.context = context;
@@ -28,14 +27,6 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
         if(listFilms == null) return;
         this.listFilms.clear();
         this.listFilms.addAll(listFilms);
-    }
-
-    public String getFilmType() {
-        return filmType;
-    }
-
-    public void setFilmType(String filmType) {
-        this.filmType = filmType;
     }
 
     public void setOnItemClickCallback(OnItemClickCallback onItemClickCallback) {
@@ -52,7 +43,7 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
     @Override
     public void onBindViewHolder(@NonNull FilmViewHolder holder, int position) {
         FilmEntity filmEntity = listFilms.get(position);
-        holder.bind(filmEntity, filmType);
+        holder.bind(filmEntity);
     }
 
     @Override
@@ -69,7 +60,7 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
         }
 
         @SuppressLint("UseCompatLoadingForDrawables")
-        void bind(FilmEntity filmEntity, String filmType){
+        void bind(FilmEntity filmEntity){
             itemFilmBinding.textViewTitle.setText(filmEntity.getTitle());
             itemFilmBinding.textViewYear.setText(filmEntity.getYear());
             itemFilmBinding.textViewRating.setText(filmEntity.getRating());
@@ -78,12 +69,12 @@ public class FilmAdapter extends RecyclerView.Adapter<FilmAdapter.FilmViewHolder
             itemFilmBinding.imageView.setImageDrawable(context.getResources().getDrawable(imageResource));
 
             itemView.setOnClickListener(v -> {
-                onItemClickCallback.onItemClicked(filmEntity, filmType);
+                onItemClickCallback.onItemClicked(filmEntity);
             });
         }
     }
 
     public interface OnItemClickCallback {
-        void onItemClicked(FilmEntity filmEntity, String filmType);
+        void onItemClicked(FilmEntity filmEntity);
     }
 }
