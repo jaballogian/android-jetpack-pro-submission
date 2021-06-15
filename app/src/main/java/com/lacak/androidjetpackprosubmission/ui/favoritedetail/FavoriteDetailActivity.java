dc5e3e58d06b7c73436859b88f1a4bfb5b4b7ba7
jaballogian <jaballogianrma77@gmail.com>
2021-06-15 21:18:04 +0700

    add Favorite Detail Activity to the app

---
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/favoritedetail/FavoriteDetailActivity.java
@@ -0,0 +1,16 @@
+package com.lacak.androidjetpackprosubmission.ui.favoritedetail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lacak.androidjetpackprosubmission.R;
+
+public class FavoriteDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_detail);
+    }
+}
