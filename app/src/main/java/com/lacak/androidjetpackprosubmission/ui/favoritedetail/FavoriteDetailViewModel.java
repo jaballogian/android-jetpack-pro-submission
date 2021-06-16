e42e4b325fff48b24ed09e1d0250c8e6c744616b
jaballogian <jaballogianrma77@gmail.com>
2021-06-16 21:21:59 +0700

    add set selected favorite movie and show functions and get selected favorite movie and show functions in Favorite Detail View Model

---
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/ui/favoritedetail/FavoriteDetailViewModel.java
@@ -0,0 +1,32 @@
+package com.lacak.androidjetpackprosubmission.ui.favoritedetail;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.lacak.androidjetpackprosubmission.data.FavoriteFilmRepository;
+import com.lacak.androidjetpackprosubmission.data.source.local.entity.FilmEntity;
+
+public class FavoriteDetailViewModel extends ViewModel {
+    private int id;
+    private FavoriteFilmRepository favoriteFilmRepository;
+
+    public FavoriteDetailViewModel(FavoriteFilmRepository favoriteFilmRepository) {
+        this.favoriteFilmRepository = favoriteFilmRepository;
+    }
+
+    public void setSelectedFavoriteMovie(int id) {
+        this.id = id;
+    }
+
+    public void setSelectedFavoriteShow(int id) {
+        this.id = id;
+    }
+
+    public LiveData<FilmEntity> getSelectedFavoriteMovie() {
+        return favoriteFilmRepository.getDetailFavoriteMovie(id);
+    }
+
+    public LiveData<FilmEntity> getSelectedFavoriteShow() {
+        return favoriteFilmRepository.getDetailFavoriteShow(id);
+    }
+}
