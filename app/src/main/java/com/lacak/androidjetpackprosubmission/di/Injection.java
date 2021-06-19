8b181c03f237786d3980f3c923d23fc87b5a6640
jaballogian <jaballogianrma77@gmail.com>
2021-06-19 19:42:12 +0700

    change provide favorite film repository function in Injection

--- app/src/main/java/com/lacak/androidjetpackprosubmission/di/Injection.java
+++ app/src/main/java/com/lacak/androidjetpackprosubmission/di/Injection.java
@@ -5,8 +5,14 @@ import android.content.Context;
 
 import com.lacak.androidjetpackprosubmission.data.FavoriteFilmRepository;
 import com.lacak.androidjetpackprosubmission.data.MainRepository;
+import com.lacak.androidjetpackprosubmission.data.source.local.LocalDataSource;
+import com.lacak.androidjetpackprosubmission.data.source.local.room.FavoriteFilmDao;
+import com.lacak.androidjetpackprosubmission.data.source.local.room.FavoriteFilmRoomDatabase;
 import com.lacak.androidjetpackprosubmission.data.source.remote.RemoteDataSource;
 import com.lacak.androidjetpackprosubmission.utils.JsonHelper;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Injection {
     public static MainRepository provideRepository(Context context){
@@ -15,6 +21,10 @@ public class Injection {
     }
 
     public static FavoriteFilmRepository provideFavoriteFilmRepository(Application application){
-        return FavoriteFilmRepository.getInstance(application);
+        FavoriteFilmRoomDatabase favoriteFilmRoomDatabase = FavoriteFilmRoomDatabase.getDatabase(application);
+        FavoriteFilmDao favoriteFilmDao = favoriteFilmRoomDatabase.favoriteFilmDao();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        LocalDataSource localDataSource = LocalDataSource.getInstance(favoriteFilmDao, executorService);
+        return FavoriteFilmRepository.getInstance(localDataSource);
     }
 }
